a92cb5f11f1bcfab9cbb60ee9947bbd55154a4c6
Eric Campbell <eric.robert.campbell@gmail.com>
2024-10-06 08:14:50 -0600

    feat: updated socials
    Updated the socials links to include mastodon and instagram. Also
    includede the links on every page.
    
    Closes #205
    Closes #206

--- src/utility/constants.ts
+++ src/utility/constants.ts
@@ -1,3 +1,12 @@
 export const APS_FACEBOOK_LINK = 'http://www.facebook.com/albertapaleosociety/'
 export const APS_YOUTUBE_LINK = 'https://www.youtube.com/@AlbertaPaleo'
+export const APS_INSTAGRAM_LINK =
+    'https://www.instagram.com/alberta_palaeo_society/'
+export const APS_MASTODON_LINK = 'https://sauropods.win/@alberta_palaeo_society'
+export const SOCIAL_MEDIA_LINKS = {
+    facebook: APS_FACEBOOK_LINK,
+    youtube: APS_YOUTUBE_LINK,
+    instagram: APS_INSTAGRAM_LINK,
+    mastodon: APS_MASTODON_LINK,
+}
 export const THREE_D_SCAN_LOCATION = '/fossilCollection/3dScans/'
