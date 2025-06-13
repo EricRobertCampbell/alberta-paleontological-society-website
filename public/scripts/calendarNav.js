70f9aae40684abe5347d70f13fc5b0f13b03abae
Eric Campbell <eric.robert.campbell@gmail.com>
2025-06-13 07:08:24 -0600

    partial progress: view transitions working on the calendar component

--- public/scripts/calendarNav.js
+++ public/scripts/calendarNav.js
@@ -2,7 +2,6 @@
  * Get the new calendar content for the specified year and month, adjusting for overflow / underflow on the month
  */
 function navigateToMonth(year, month) {
-	console.log('Bang!')
 	// Adjust year/month if month goes out of bounds
 	if (month < 1) {
 		month = 12
@@ -14,23 +13,35 @@ function navigateToMonth(year, month) {
 
 	const url = `/events/calendar?year=${year}&month=${month}&partial=true`
 
-	fetch(url)
-		.then((res) => res.text())
-		.then((html) => {
-			const parser = new DOMParser()
-			const doc = parser.parseFromString(html, 'text/html')
-			const newContent = doc.querySelector('#calendar-content')
-			const oldContent = document.querySelector('#calendar-content')
-			if (newContent && oldContent) {
-				oldContent.replaceWith(newContent)
-				history.pushState(
-					{},
-					'',
-					`/events/calendar?year=${year}&month=${month}`
-				)
-			}
-		})
-		.catch((err) => console.error('Calendar update failed:', err))
+	if (!document.startViewTransition) {
+		console.log('No view transition found!')
+		fetch(url)
+			.then((res) => res.text())
+			.then((html) => updateCalendarContent(html, year, month))
+			.catch((err) => console.error('Calendar update failed:', err))
+		return
+	}
+	document.startViewTransition(async () => {
+		const res = await fetch(url)
+		const html = await res.text()
+		updateCalendarContent(html, year, month)
+	})
+}
+
+const updateCalendarContent = (html, year, month) => {
+	const parser = new DOMParser()
+	const doc = parser.parseFromString(html, 'text/html')
+	const newContent = doc.querySelector('#calendar-content')
+	const oldContent = document.querySelector('#calendar-content')
+
+	if (newContent && oldContent) {
+		oldContent.replaceWith(newContent)
+		history.pushState(
+			{},
+			'',
+			`/events/calendar?year=${year}&month=${month}`
+		)
+	}
 }
 
 window.navigateToMonth = navigateToMonth
