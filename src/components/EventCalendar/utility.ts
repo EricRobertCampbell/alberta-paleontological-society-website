332f25e052336090138e37c5ca7bf087b104985b
Eric Campbell <eric.robert.campbell@gmail.com>
2025-06-21 19:49:19 -0600

    feat: added legend to event calendar
    Added a legend explaining what each of the colours represents.

---
+++ src/components/EventCalendar/utility.ts
@@ -0,0 +1,18 @@
+export const getEventTypeClass = (eventType: string) => {
+	switch (eventType) {
+		case 'Monthly Meeting':
+			return 'event-monthly-meeting'
+		case 'Symposium':
+			return 'event-symposium'
+		case 'Field Trip':
+			return 'event-field-trip'
+		case 'Special Joint Meeting of the APS and the CSPG BASS Division':
+			return 'event-default'
+		case 'Fossil Sorting':
+			return 'event-fossil-sorting'
+		case 'External':
+			return 'event-external'
+		default:
+			return 'event-default'
+	}
+}
