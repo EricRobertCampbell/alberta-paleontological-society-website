bbf9240053d40d2c7e64ea9ee967a48c541e565f
Eric Campbell <eric.robert.campbell@gmail.com>
2025-09-04 06:59:12 -0600

    feat: added bsky social media links
    Added the social media links for Bluesky.

--- src/utility/constants.ts
+++ src/utility/constants.ts
@@ -3,11 +3,13 @@ export const APS_YOUTUBE_LINK = 'https://www.youtube.com/@AlbertaPaleo'
 export const APS_INSTAGRAM_LINK =
     'https://www.instagram.com/alberta_palaeo_society/'
 export const APS_MASTODON_LINK = 'https://sauropods.win/@alberta_palaeo_society'
+export const APS_BSKY_LINK = 'https://bsky.app/profile/abpalaeosociety.bsky.social'
 export const SOCIAL_MEDIA_LINKS = {
     facebook: APS_FACEBOOK_LINK,
     youtube: APS_YOUTUBE_LINK,
     instagram: APS_INSTAGRAM_LINK,
     mastodon: APS_MASTODON_LINK,
+    bsky: APS_BSKY_LINK,
 }
 export const THREE_D_SCAN_LOCATION = '/fossilCollection/3dScans/'
 
